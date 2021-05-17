a7d2d03b9e59f066ea342cd499a5d51b3bdc3f3e
prosvetovvv <vp12@yandex.ru>
2021-05-17 10:11:38 +0300

    Initial Commit

---
+++ CurrencyRateTestForMKB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CurrencyRateTestForMKB
+//
+//  Created by Vitaly Prosvetov on 17.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
