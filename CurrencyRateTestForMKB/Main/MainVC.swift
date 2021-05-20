6476af41ee0217ae9c55e9c35d6fc6ad135ff030
prosvetovvv <vp12@yandex.ru>
2021-05-20 11:25:37 +0300

    Add RateView. Refactoring MainVC.

---
+++ CurrencyRateTestForMKB/Main/MainVC.swift
@@ -0,0 +1,74 @@
+//
+//  MainTableVC.swift
+//  CurrencyRateTestForMKB
+//
+//  Created by Vitaly Prosvetov on 17.05.2021.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+    private let rootView = RateView()
+    private var rates = [Rate]()
+    
+    // MARK: - Lifecycle
+    
+    override func loadView() {
+        super.loadView()
+        view = rootView
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupSelf()
+        setupTableView()
+        getExchangeRates()
+    }
+    
+    // MARK: - Private
+    
+    private func setupSelf() {
+        view.backgroundColor = .systemBackground
+    }
+    
+    private func setupTableView() {
+        rootView.tableView.register(RateCell.self, forCellReuseIdentifier: RateCell.reuseID)
+        rootView.tableView.dataSource = self
+    }
+    
+    private func getExchangeRates() {
+        NetworkService.shared.getExchangeRates { result in
+            switch result {
+            case .success(let rates):
+                DispatchQueue.main.async {
+                    self.rates = rates
+                    self.rootView.tableView.reloadData()
+                }
+            case .failure(let error):
+                self.presentCRAlertOnMainThread(title: "Что-то пошло не так", message: error.rawValue, buttonTitle: "Ok", completionHandler: nil)
+            }
+        }
+    }
+}
+
+// MARK: - Extensions
+
+extension MainVC: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        rates.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let rate = rates[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: RateCell.reuseID, for: indexPath) as! RateCell
+        
+        cell.set(with: rate)
+        
+        return cell
+    }
+}
---
+++ CurrencyRateTestForMKB/Main/RateView.swift
@@ -0,0 +1,45 @@
+//
+//  RateView.swift
+//  CurrencyRateTestForMKB
+//
+//  Created by Vitaly Prosvetov on 20.05.2021.
+//
+
+import UIKit
+
+class RateView: UIView {
+    let tableView = UITableView()
+    
+    // MARK: - Init
+    
+    init() {
+        super.init(frame: .zero)
+        setupSelf()
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    // MARK: - Private
+    
+    func setupSelf() {
+        tableView.rowHeight = 95
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        addSubview(tableView)
+        
+        setNeedsUpdateConstraints()
+    }
+    
+    // MARK: - Layout
+    
+    override func updateConstraints() {
+        super.updateConstraints()
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor)
+        ])
+    }
+}
